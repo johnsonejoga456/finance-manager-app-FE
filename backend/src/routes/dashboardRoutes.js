6ebfe6af83b7b9e28d9b4177ecc20cc840a301c9
johnsonejoga456 <ejogajohnson@gmail.com>
2025-04-01 22:17:02 +0100

    A major separation

--- backend/src/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getDashboardData } from '../controllers/dashboardController.js';
-import authMiddleware from '../middleware/auth.js';
-
-const dashboardRouter = express.Router();
-
-dashboardRouter.get('/', authMiddleware, getDashboardData);
-
-export default dashboardRouter;
